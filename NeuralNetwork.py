95635416cd924803daf54e88fb373662e4a66ca8
Nucelles <An@nindeu@2019>
2019-10-31 15:55:55 +0100

    - neuronTestCases.py code has been modified to test for the two test cases that were sent in by Prof. Navet.
    - NeuralNetwork.py code has been heavily modified the main changes are;
    --> added outputLayer as a subclass of layer
    --> added a new class network to manage the layers and neurons better.
    --> this new class provided functionality such as updating activation functions and weights

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,36 +3,57 @@ Hello World
 """
 
 """
-Below is the defintion of the Neuron class
+NEURON CLASSES
 """
 import numpy as np
 import math
+from statistics import mean
+
+def derivitiveSigmoid(x):
+    """
+    This function returns the value inputted into the derivative sigmoid function.
+    :param x: float, value that will passedi nto the function
+    :return:
+    """
+
+    derivitiveX = 1 / (1 + math.exp(-x))
+
+    return derivitiveX * (1 - derivitiveX)
+
+
 
 class Neuron:
 
     def __init__(self, numberOfInputs, activationFunction):
         self.activationFunction = activationFunction
-        self.weights = np.random.rand(numberOfInputs)
-        self.weightsPreset = []
-        self.bias = 0
+        self.weights = [0,1]
+        self.bias = 1
         self.output = 0
+
+    def defineWeights(self, numberOfWeights):
+        """
+        This function defines the randomized weights for the neuron
+        :param numberOfWeights:
+        :return:
+        """
+        self.weights = np.random.rand(numberOfWeights)
 
     def runNeuron(self, inputs):
         """
         This function will run the other functions of the neurons, all the necessary functions to process the input.
+        :param inputs: list of floats, the array of previous neuron outputs of the last layer
         :return:
         """
         dotOutput = self.applyDot(inputs)
         output = self.applyActivationFunction(dotOutput)
-        # print(dotOutput)
-        # print(output)
 
         return output
 
     def applyDot(self, inputs):
         """
         This function use the dot function to on the input and weights.
-        Input = [Inputs]
+        :param inputs: list of floats, the array of previous neuron outputs of the last layer
+        :return:
         """
 
         dotOutput = 0
@@ -43,8 +64,9 @@ class Neuron:
 
     def applyActivationFunction(self, dotOutput):
         """
-        This function will apply the sigmoid function to the sum of the inputs.
-        Input = SumOfInputs, Output = finalOutput
+        This function will apply the sigmoid function to the sum of the inputs and retunr the nuerons output.
+        :param dotOutput: float, this is the value that is returned from the applyDot() function
+        :return:
         """
         if self.activationFunction == "Sigmoid":
             self.output = 1 / (1 + math.exp(-dotOutput))
@@ -71,40 +93,104 @@ class Neuron:
 Below is the definition of the layer class 
 """
 
+
 class Layer:
     def __init__(self, layers, activationFunction):
         self.layers = layers
         self.neuronList = []
         self.layerOutput = []
+        self.previousLayer = None
+        self.followingLayer = None
 
         for i in range(self.layers):
             self.neuronList.append(Neuron(layers, activationFunction))
+
+    def changeActivationFunction(self, activationFunction):
+        """
+        This function will change the activation function of the layer, and return nothing
+        :param activationFunction: string, this is the code of the activation function, see code documentation
+        :return:
+        """
+        for i in self.neuronList:
+            i.activationFunction = activationFunction
 
 
 class NeuronLayer(Layer):
 
     def __init__(self, layers, activationFunction):
-        self.previousLayer = None
         super().__init__(layers, activationFunction)
 
     def runLayer(self):
+        """
+        This function will loop through all the neurons of the layer and activate the runNeuron() function.
+        :return:
+        """
         self.layerOutput = []
         for neuron in self.neuronList:
             self.layerOutput.append(neuron.runNeuron(self.previousLayer.layerOutput))
 
+    def updateWeights(self, learningRate, networkOutput, expectedOutput):
+        """
+        This function is part of the back-propagation code. This function should run through each neuron & update their
+        base weights and biases
+        :param learningRate: flaot, the defined learning rate of the neural network
+        :param networkOutput: float, the output of the neural network
+        :param expectedOutput: float, this is the expected output of the neural network
+        :return:
+        """
+        for n in range(len(self.neuronList)):
+            for i in range(len(self.neuronList[n].weights)):
+                Part1 = (-2 * (networkOutput[0] - expectedOutput))
+                Part2 = (self.followingLayer.neuronList[i].weights[i] * derivitiveSigmoid(sum(self.followingLayer.layerOutput)))
+                Part3 = (self.previousLayer.layerOutput[i] * derivitiveSigmoid(self.layerOutput[0]))
+                self.neuronList[n].weights[i] -= learningRate * Part1 * Part2 * Part3
+
 
 class inputLayer(Layer):
-    def __init__(self, inputSize, inputData, activationFunction):
-        super().__init__(inputSize, activationFunction)
+    def __init__(self, layerSize, activationFunction):
+        super().__init__(layerSize, activationFunction)
 
+    def runLayer(self, inputData):
+        """
+        This function will loop through all the neurons of the layer, but instead of running the runNeuron layer it will
+        change the output of each neuron.(The number of neurons in the layer must match the size of the inputData array)
+        :param inputData: list of floats, This is the unchanged input data that will be feed to the neuron.
+        :return:
+        """
         for i in range(self.layers):
             self.neuronList[i].output = inputData[i]
             self.layerOutput.append(inputData[i])
 
+class outputLayer(Layer):
+    def __init__(self, layerSize, activationFunction):
+        super().__init__(layerSize, activationFunction)
+
     def runLayer(self):
+        """
+        This function will loop through all the neurons of the layer and activate the runNeuron() function.
+        :return:
+        """
         self.layerOutput = []
         for neuron in self.neuronList:
-            self.layerOutput.append(neuron.applyActivationFunction())
+            self.layerOutput.append(neuron.runNeuron(self.previousLayer.layerOutput))
+
+    def updateWeights(self, learningRate, networkOutput, expectedOutput):
+        """
+        This function is part of the back-propagation code. This function should run through each neuron & update their
+        base weights and biases
+        :param learningRate: flaot, the defined learning rate of the neural network
+        :param networkOutput: float, the output of the neural network
+        :param expectedOutput: float, this is the expected output of the neural network
+        :return:
+        """
+        for neuron in self.neuronList:
+            for i in range(len(neuron.weights)):
+                Part1 = (-2 * (networkOutput - expectedOutput))
+                Part3 = (self.previousLayer.layerOutput[i] * derivitiveSigmoid(self.layerOutput))
+                neuron.weights[i] -= learningRate * Part1 * Part3
+
+
+
 
 
 """
@@ -122,6 +208,20 @@ class Network:
                 assert issubclass(type(i), Layer)
                 self.layers.append(i)
 
+
+    def feedNetwork(self, x):
+        """
+        This function will feed an input through the network once, without back-propagation
+        :param x: list of floats, this is the input for the network
+        :return:
+        """
+
+        self.layers[0] = self.layers[0].runLayer(x)
+        for layer in self.layers[1:]:
+            layer.runLayer()
+
+        return self.layers[-1].layerOutput[0]
+
     def add(self, layerToAdd):
         """
         This function wil add a layer to the network in order. Only one layer can be added at a time.
@@ -130,28 +230,43 @@ class Network:
         """
         assert issubclass(type(layerToAdd), Layer)
         self.layers.append(layerToAdd)
+        if type(layerToAdd) != inputLayer:
+            self.layers[-1].previousLayer = self.layers[-2]
+            self.layers[-2].followingLayer = self.layers[-1]
 
-    def compile(self):
-        """
-        This function will connect each layer of the neuron and will check that the first layer is an inputLayer
-        :return:
-        """
-        if type(self.layers[0]) == inputLayer:
-            for i in range(len(self.layers), 0, -1):
-                self.layers[i].previousLayer = self.layers[i-1]
-        else:
-            print("Error: The first layer of the network must be an input layer.")
-            return
+            #for neuron in self.layers[-1].neuronList:
+             #   neuron.defineWeights(len(self.layers[-2].neuronList))
 
-    def runNetwork(self, data):
+    def runNetwork(self, inputData, learningRate, epochs):
         """
         This function will receive the training set and run the neural network.
-        ;param data: This is the list of data that will be fed to the neural network.
+        :param data: This is the list of data that will be fed to the neural network.
         :return:
         """
+        if len(self.layers[0].neuronList) != len(inputData[0]):
 
-        for layer in self.layers:
-            layer.runLayer()
+            for currentEpoch in range(epochs):
+                for currentX, predictedY in zip(inputData[0], inputData[1]):
+
+                    self.layers[0] = self.layers[0].runLayer(currentX)
+
+                    for layer in self.layers[1:]:
+                        layer.runLayer()
+
+                    networkOutput = self.layers[-1].layerOutput
+
+                    """
+                    for layer in self.layers[1:]:
+                        layer.updateWeights(learningRate, networkOutput, predictedY)
+                    """
+
+                if epochs % 10 == currentEpoch:
+                    print("At epoch %" % currentEpoch)
+                    yPredictions = [self.feedNetwork(x) for x in inputData[0]]
+                    loss = self.meanSquaredLoss(networkOutput, yPredictions)
+
+        else:
+            print("The amount of neurons in the input layer does not match the size of the x input.")
 
     def saveWeights(self):
         """
@@ -168,6 +283,32 @@ class Network:
             layerWeights = []
 
         return networkWeights
+
+    def inputWeights(self, presetWeights):
+        """
+        This function will receive a structured array of weights which it will input into each layer of the network
+        :param presetWeights: list of list of float, this is a structured array of weights that is needed adding a
+        predefined weight.
+        :return:
+        """
+        for layer in range(len(self.layers)):
+            for neuron in range(len(self.layers[layer].neuronList)):
+                self.layers[layer].neuronList[neuron].weights = presetWeights[layer][neuron]
+
+    def meanSquaredLoss(self, output, actual):
+        """
+        This function will calculate the mean squared loss of a neuron.
+        :param output: This is the output of the model
+        :param actual: This is the actual intended output
+        :return:
+        """
+        if len(output) == len(actual):
+            meanList = [(output[i]-actual[i])**2 for i in range(len(output))]
+            return mean(meanList)
+        else:
+            print("The output and actual results length do not match.")
+
+        return
 
 
 
--- neuronTestCases.py
+++ neuronTestCases.py
@@ -2,6 +2,54 @@
 
 # The test cases need to be changed for the new network class format
 
+import NeuralNetwork as nn
+"""
+This is the Test Case for the first link, https://enlight.nyc/projects/neural-network/
+"""
+
+modelB = nn.Network()
+
+modelB.add(layerToAdd=nn.inputLayer(2, "Sigmoid"))
+modelB.add(layerToAdd=nn.NeuronLayer(3, "Sigmoid"))
+modelB.add(layerToAdd=nn.outputLayer(1, "Sigmoid"))
+
+presetWeights = [[], []], [[.2, .6], [.1, .8], [.3, .7]], [[.4, .5, .9]]
+modelB.inputWeights(presetWeights)
+
+print("Test Case 1")
+print("Expected = 0.8579443067")
+print("Network =", modelB.feedNetwork([2, 9]))
+print("")
+
+
+"""
+This is the Test Case for the second link, https://victorzhou.com/blog/intro-to-neural-networks/
+"""
+
+modelA = nn.Network()
+
+modelA.add(layerToAdd=nn.inputLayer(2, "Sigmoid"))
+modelA.add(layerToAdd=nn.NeuronLayer(2, "Sigmoid"))
+modelA.add(layerToAdd=nn.outputLayer(1, "Sigmoid"))
+
+presetWeights = [[], []], [[0, 1], [0, 1]], [[0, 1]]
+modelA.inputWeights(presetWeights)
+
+print("Test Case 2")
+print("Expected = 0.7216325609518421")
+print("Network  =", modelA.feedNetwork([2, 3]))
+
+"""
+Used for debugging the layers in the model
+
+print("\nLayers in network")
+for i in model.layers:
+  print("\n", i)
+  print("%a is the previous layer." % i.previousLayer)
+  print("%a is the following layer." % i.followingLayer)
+"""
+
+
 
 """
 inputlayer1 = layer.inputLayer(5, trainingInput, "Sigmoid")
